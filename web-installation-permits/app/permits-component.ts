39b27d9b1aa93f01e6907ed050ff44ab322ccd56
dtorresf <dtorresf@redhat.com>
2016-10-26 20:12:03 -0600

    refactor signals for stop human task

--- web-installation-permits/app/permits-component.ts
+++ web-installation-permits/app/permits-component.ts
@@ -5,55 +5,6 @@ import { PermitService } from './permit.service';
 
 @Component({
   selector: 'solar-village-permits',
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .permits {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .permits li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .permits li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .permits li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .permits .text {
-      position: relative;
-      top: -3px;
-    }
-    .permits .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `],
   template:`
     <h2>In progress permit requests</h2>
     <ul class="permits">
